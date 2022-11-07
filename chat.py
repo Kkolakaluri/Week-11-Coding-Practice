d57fe52d4632b62580e6172205a767ddbedc7bdb
Kkolakaluri <116460706+Kkolakaluri@users.noreply.github.com>
2022-11-06 22:33:16 -0600

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,31 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+class Chat_T:
+    chatbot: None
+
+    def __init__(self):
+        self.chatbot = ChatBot("ChatBot")
+    # Training with Personal Ques & Ans
+        conversation = [
+            "Hello",
+            "Hi there!",
+            "How are you doing?",
+            "I'm doing great.",
+            "You're welcome.",
+            "who Developed you",
+            "NLP class",
+            "what's your favorite color?",
+            "Green",
+            "what do you like to do?",
+            "Chat",
+            "what is kind of conversations do you like?",
+            "personal",
+        ]
+        trainer = ListTrainer(self.chatbot)
+        trainer.train(conversation)
+
+    def chat(self, t1):
+        response = self.chatbot.get_response(t1)
+        return response
